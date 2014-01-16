44338e69e04cf1672f433dc2e510cdcb30d6dcb4
keitainazu <keitainazu@gmail.com>
2014-01-17 00:26:58 +0900

    re create repository

--- GPSSample/ViewController.h
+++ GPSSample/ViewController.h
@@ -7,7 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController : UIViewController
-
+@interface ViewController : UIViewController <CLLocationManagerDelegate> {
+    CLLocationManager*lm;
+}
+@property (weak, nonatomic) IBOutlet UILabel *latitude;
+@property (weak, nonatomic) IBOutlet UILabel *longitude;
+@property (weak, nonatomic) IBOutlet UIImageView *compass;
 @end
--- GPSSample/ViewController.m
+++ GPSSample/ViewController.m
@@ -18,12 +18,68 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    //位置情報マネージャを生成して設定
+    lm =[[CLLocationManager alloc] init];
+    lm.delegate =self;
+    
+    //位置情報マネージャーの精度を設定
+    [self setDesiredAccuracy];
+    
+    //位置情報を更新する距離
+    lm.distanceFilter =kCLDistanceFilterNone;
+    //位置情報の取得の開始
+    [lm startUpdatingLocation];
+    [lm startUpdatingHeading];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+//デリゲートメソッド
+//現在の位置を取得する
+- (void) locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    //位置情報が取得されたら行う処理
+    //比較的新しいイベントの場合は、節電のために更新をオフにする
+    CLLocation *location = [locations lastObject];
+    NSDate *eventData = location.timestamp;
+    NSTimeInterval howRecent = [eventData timeIntervalSinceNow];
+    
+    if(abs(howRecent)< 15.0){
+        //緯度取得
+        _latitude.text = [NSString stringWithFormat:@"%g", location.coordinate.latitude];
+        //経度取得
+        _longitude.text = [NSString stringWithFormat:@"%g", location.coordinate.longitude];
+        
+    }
+}
+
+//デリゲートメソッド
+//方角情報を取得する
+-(void) locationManager:(CLLocationManager *)manager didUpdateHeading:(CLHeading *)newHeading {
+
+    //方角が取得されたら行う処理
+    _compass.transform = CGAffineTransformMakeRotation(- newHeading.magneticHeading * M_PI/180.0f);
+}
+
+//位置情報マネージャーの精度を設定
+- (void)setDesiredAccuracy  {
+    //最高の精度
+    //lm.desiredAccuracy =kCLLocationAccuracyBest;
+    
+    //10Mの精度
+    //lm.desiredAccuracy =kCLLocationAccuracyNearestTenMeters;
+    
+    //100の精度
+    //lm.desiredAccuracy =kCLLocationAccuracyHundredMeters;
+    
+    //1Kmの精度
+    //lm.desiredAccuracy =kCLLocationAccuracyKilometer;
+    
+    //3Kmの精度
+    lm.desiredAccuracy =kCLLocationAccuracyThreeKilometers;
 }
 
 @end
